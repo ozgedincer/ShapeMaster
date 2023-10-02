85187c680c26250ea73084857e416ecf48ad275c
Özge Dinçer <dncerozge@gmail.com>
2023-10-02 20:33:12 +0300

    Proje dosyası ekle.

---
+++ Core/ShapeMaster.Domain/Entities/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeMaster.Domain.Common;
+
+namespace ShapeMaster.Domain.Entities
+{
+    internal class Rectangle : Shape
+    {
+        public decimal ASide { get; set; }
+        public decimal BSide { get; set; }
+
+        public decimal GetArea()
+        {
+            return ASide*BSide;
+        }
+    }
+}
---
+++ Core/ShapeMaster.Domain/Entities/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using ShapeMaster.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeMaster.Domain.Entities
+{
+    internal class Triangle : Shape
+    {
+        public decimal Base { get; set; }
+        public decimal Height { get; set; }
+
+        public decimal GetArea()
+        {
+            return (Base*Height)/2 ;
+        }
+    }
+}
